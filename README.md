1502fcba3fcaeebcaac5ef0ca8bfea8bdb773ce3
Bennie Mosher <benniemosher@gmail.com>
2024-10-22 06:34:54 -0600

    Added config to GPG sign git commits

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 
 2024 rendition using chezmoi, starship, and lazyvim.
 
+## Usage
+
+Create a `~/.profile.local` filling in contents below:
+
+```
+# ~/.profile.local
+
+export KEYBASE_USERNAME="<ENTER_USERNAME>"
+export KEYBASE_PAPERKEY="<ENTER_PAPERKEY>"
+export KEYBASE_KEY_ID="<ENTER_KEY_ID>"
+```
---
+++ run_onchange_install-gpg-keys.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+GNUPG_HOME="$HOME/.gnupg"
+if [ -f "$GNUPG_HOME" ]; then
+    if [ "$(stat -c %a "$GNUPG_HOME")" != "700" ] ; then
+        chmod 700 "$GNUPG_HOME"
+    fi
+fi
+
+HOSTNAME=$(hostname -s)
+keybase --pinentry=none login $KEYBASE_USERNAME --paperkey "$KEYBASE_PAPERKEY" --devicename "$HOSTNAME" 
+
+gpgconf --kill gpg-agent
+
+keybase pgp export -q "$KEYBASE_KEY_ID" | gpg --import
+
+keybase --pinentry=none pgp export -q "$KEYBASE_KEY_ID" --secret | gpg --allow-secret-key-import --import
