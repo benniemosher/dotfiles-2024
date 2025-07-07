96a79a88413766db07bdf27cc6837970310dabfc
Bennie Mosher <benniemosher@gmail.com>
2025-07-06 19:27:30 -0600

    feat: Update with changes from day job

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -6,12 +6,13 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
--- For example, changing the color scheme:
+-- Change the color scheme
 config.color_scheme = "nord"
-
 config.window_background_opacity = 0.95
 
+-- Change the font
 config.font = wezterm.font("Hack Nerd Font Mono")
+config.font_size = 13.0
 
 -- and finally, return the configuration to wezterm
 return config
